d9f9d31a4206952906fdaa8721c753b9b94beedb
jjmin9797 <jjmin9797@naver.com>
2023-02-16 22:14:05 +0900

    feat: 크롤링 공부

---
+++ 크롤링/scraping.py
@@ -0,0 +1,34 @@
+"""
+웹 크롤링 : 검색 엔진의 구축 등을 위하여 특정한 방법으로 웹 페이지를 수집하는 프로그램
+웹 스크래핑 : 웹에서 데이터를 수집하는 프로그램
+"""
+from bs4 import BeautifulSoup
+import aiohttp
+import asyncio
+
+
+async def fetch(session, url):
+    async with session.get(url) as response:
+        html = await response.text()
+        soup = BeautifulSoup(html, "html.parser")
+        cont_thumb = soup.find_all("div", "cont_thumb")
+        print(cont_thumb)
+        for cont in cont_thumb:
+            try:
+                title = cont.find("p", "txt_thumb").text
+                print(title)
+            except:
+                print("제목 없음")
+
+
+async def main():
+    BASE_URL = "https://bjpublic.tistory.com/category/%EC%A0%84%EC%B2%B4%20%EC%B6%9C%EA%B0%84%20%EB%8F%84%EC%84%9C"
+    urls = [f"{BASE_URL}?pages={i}" for i in range(1, 10)]
+    async with aiohttp.ClientSession(
+        connector=aiohttp.TCPConnector(ssl=False)
+    ) as session:
+        await asyncio.gather(*[fetch(session, url) for url in urls])
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
